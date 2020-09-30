6f3e7131b40e274f83cd597caa1da451784c95d3
Kunal Chauhan <65803293+Kunal-Chauhan@users.noreply.github.com>
2020-09-30 22:23:53 +0530

    Update test_main.cpp

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -21,12 +21,13 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
-
+//includeing library files
 #include <catch/catch.hpp>
-
+//main function
 int unittest_main(int argc, char *argv[]);
-
+//running test case
 TEST_CASE("main: success")
 {
+    //processing function
     CHECK_NOTHROW(unittest_main(0, nullptr));
 }
